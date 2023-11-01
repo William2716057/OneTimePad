ebacbd3ea6b3e7e40da60f9c4eaaffeb6a50ba6f
William Spriggs <Willspri@gmail.com>
2023-11-01 14:42:42 +1300

    changed

--- OneTimePad/OneTimePad.cpp
+++ OneTimePad/OneTimePad.cpp
@@ -1,9 +1,63 @@
 // OneTimePad.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
+
+
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+
+void generateKey(int length) {
+	//writes key into a text file
+	std::ofstream keyFile("key.txt");
+
+	for (int i = 0; i < length; i++) {
+		//ensure that the result is between 0 and 255
+		char randomChar = rand() % 256;
+		keyFile << randomChar;
+	}
+
+	keyFile.close();
+}
+
+//read plaintext from which to create key
+
+std::string readPlaintext(const std::string& filename) {
+	std::string plaintext;
+	std::ifstream file(filename);
+	if (file.is_open()) {
+		char c;
+		while (file.get(c)) {
+			plaintext += c;
+		}
+		file.close();
+	}
+	return plaintext;
+}
+
+//encrypt plaintext
+std::string encrypt(const std::string& plaintext, const std::string& key) {
+	std::string ciphertext;
+	for (size_t i = 0; i < plaintext.size(); i++) {
+		char encryptedChar = plaintext[i] ^ key[i];
+		ciphertext += encryptedChar;
+	}
+	return ciphertext;
+}
+//decrypt 
+std::string decrypt(const std::string& ciphertext, const std::string& key) {
+	return encrypt(ciphertext, key);
+}
+
+
+void writeCiphertext(const std::string& ciphertext, const std::string& filename) {
+	std::ofstream file(filename);
+	if (file.is_open()) {
+		file << ciphertext;
+		file.close();
+	}
+}
 
 
 #include <iostream>
@@ -24,6 +78,14 @@ int main()
 
     //write ciphertext
     writeCiphertext(ciphertext, "ciphertext.txt");
+
+    //decrypt
+    std::string decryptedText = decrypt(ciphertext, key);
+
+    //print
+    std::cout << "decrypted: " << decryptedText << std::end1;
+
+    return 0;
 }
 
 
