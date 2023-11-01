42824d961b78c122cbfa995b2fd32d45df43baf5
William Spriggs <Willspri@gmail.com>
2023-11-01 15:11:24 +1300

    changed

--- OneTimePad/OneTimePad.cpp
+++ OneTimePad/OneTimePad.cpp
@@ -26,12 +26,16 @@ std::string readPlaintext(const std::string& filename) {
 	//store contents of file
 	std::string plaintext;
 	std::ifstream file(filename);
-
+	//on condition that file opened successfully 
 	if (file.is_open()) {
+		//defines c to temporarily store each character from file
 		char c;
+		//while loop will continue as long as file.get(c) succeeds 
 		while (file.get(c)) {
+			//append each character to plaintext
 			plaintext += c;
 		}
+		//file needs to be closed after reading
 		file.close();
 	}
 	return plaintext;
