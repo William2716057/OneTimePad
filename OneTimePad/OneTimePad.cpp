e3b36cd615183cf023aebe0782326bd6b8c688c5
William Spriggs <Willspri@gmail.com>
2023-11-01 13:55:44 +1300

    Add project files.

---
+++ OneTimePad/OneTimePad.cpp
@@ -0,0 +1,16 @@
+// OneTimePad.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
